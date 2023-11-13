a30865f7787c730c308c5770713022a1c67f01c2
fraumarzhuk <fraumarzhuk@gmail.com>
2023-11-13 22:37:44 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mariannazhukova <mariannazhukova@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:15:44 by mzhukova          #+#    #+#             */
-/*   Updated: 2023/11/13 19:34:14 by mzhukova         ###   ########.fr       */
+/*   Updated: 2023/11/13 22:24:33 by mariannazhu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 
 size_t	ft_strlen(const char *s);
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	dst_len;
 	size_t	src_len;
 	size_t	i;
 
-	src_len = 0;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i] != '\0' && (dst_len + i) < (dstsize - 1))
 	{
-		src_len++;
+		dst[dst_len + i] = src[i];
 		i++;
 	}
-	i = 0;
-	
-	
-	
+	if (dst_len + i < dstsize)
+	{
+		dst[dst_len + i] = '\0';
+	}
+	return (dst_len + src_len);
 }
